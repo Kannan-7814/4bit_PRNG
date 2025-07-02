8aaf501dc8031f263fc464f9ef1671d670493fed
Thamaraikannan T <thamaraikannan1765@gmail.com>
2025-07-02 10:11:44 +0530

    Update prng_tb.v

--- tb/prng_tb.v
+++ tb/prng_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.03.2025 22:07:53
-// Design Name: 
-// Module Name: prng_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 module prng_tb;
 
     // Testbench signals
